c6f15270f5d45f45329786679c518b677c5ecf5f
mohit <mohitleo9@gmail.com>
2017-03-20 12:50:56 +0530

    some static mockup stuff

--- src/components/PlacesPage.jsx
+++ src/components/PlacesPage.jsx
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom';
 
 
 const MainButtonBar = () =>
-  <div className="row">
+  <div style={{paddingBottom: '100px'}} className="row">
     <div className="btn-group btn-group-justified">
       <NavLink to="/t/bars/" activeClassName="active" className="btn btn-default">Bars</NavLink>
       <NavLink to="/t/resturants" className="btn btn-default">Resturants</NavLink>
@@ -11,20 +11,34 @@ const MainButtonBar = () =>
     </div>
   </div>;
 
-const HomeButton = ({text}) => (
-  <a className="btn btn-default" type="submit">{text}</a>
-);
-
 const HelloWorld = ({text}) =>
   <div className="row">
     <h1>{text || 'Places Page'}</h1>
+  </div>;
+
+const Place = ({name}) =>
+  <div style={{paddingBottom: '30px'}} className="row">
+    <img src="http://esq.h-cdn.co/assets/cm/15/06/54d3cdbba4f40_-_esq-01-bar-lgn.jpg" className="img-rounded col-md-4" width="304" height="236" />
+    <div>
+      <h3> {name} </h3>
+      <div><h2>asdfasdf</h2></div>
+      <div>mangostreet Serbia</div>
+    </div>
+  </div>;
+
+const Places = () =>
+  <div className="container">
+    <Place name="bar1" />
+    <Place name="bar2" />
+    <Place name="bar3" />
   </div>;
 
 const PlacesPage = ({match}) =>{
   return (
   <div className='container'>
     <HelloWorld text={match.params.type}/>
-    <MainButtonBar />
+    <MainButtonBar  />
+    <Places />
   </div>
   );
   }
