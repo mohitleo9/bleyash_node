cee03411781100725219e6b0ff9ccc2c8ff7d8f6
mohit <mohitleo9@gmail.com>
2017-03-22 22:04:48 +0530

    error handling

--- app/server.js
+++ app/server.js
@@ -28,10 +28,27 @@ app.use(session({
 // static files
 app.use(express.static(path.join(__dirname + '/build')));
 
-
-
+// spa app so whatever!
 app.get('/', (req, res) =>{
   res.render('index');
 })
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.send('error' + JSON.stringify(err));
+});
 
 app.listen(port);
