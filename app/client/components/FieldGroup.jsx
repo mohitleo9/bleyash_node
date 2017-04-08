361b32dfc2cd73163b016d0dd3ffa2c27e79735b
mohit <mohitleo9@gmail.com>
2017-04-08 17:11:00 +0530

    form validation

--- app/client/components/FieldGroup.jsx
+++ app/client/components/FieldGroup.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
 import {FormGroup, ControlLabel, FormControl, HelpBlock} from 'react-bootstrap';
-const FieldGroup = ({ id, label, help, ...props}) => {
-  return (
-    <FormGroup controlId={id}>
-      <ControlLabel>{label}</ControlLabel>
-      <FormControl {...props} />
-      {help && <HelpBlock>{help}</HelpBlock>}
-    </FormGroup>
-  );
-};
+
+class FieldGroup extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isDirty: false,
+      lastValue: props.value
+    };
+    this.validationState = this.validationState.bind(this);
+  }
+  componentWillReceiveProps(nextProps){
+    if (!this.state.isDirty && this.state.lastValue !== nextProps.value){
+      this.setState({isDirty: true});
+    }
+  }
+  validationState(value){
+    if (this.state.isDirty){
+      return value ? null : 'error';
+    }
+    else {
+      return null;
+    }
+  }
+  render(){
+    const { id, label, help, ...props} = this.props;
+    return (
+      <FormGroup validationState={this.validationState(props.value)} controlId={id}>
+        <ControlLabel>{label}</ControlLabel>
+        <FormControl {...props} />
+        {help && <HelpBlock>{help}</HelpBlock>}
+      </FormGroup>
+    );
+  }
+}
 
 export default FieldGroup;
